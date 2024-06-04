cb65633341641ccf992d1f992492e71585ec206d
lylim10040712 <lylim10040712@gmail.com>
2024-06-04 19:23:45 +0900

    first commit

--- Project/src/main/java/com/example/project/dto/UserDto.java
+++ Project/src/main/java/com/example/project/dto/UserDto.java
@@ -4,9 +4,11 @@ package com.example.project.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserDto {
     private String userID;
